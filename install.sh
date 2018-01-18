70e4335ee5fee1cfd0ce4b53705b7a1a2c2e05a8
Felix Rupp <kontakt@felixrupp.com>
2018-01-18 17:56:39 +0100

    Change PHP version to 7.1

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ sudo composer self-update
 
 sudo apt update;
 sudo apt --assume-yes upgrade;
-sudo apt --assume-yes install php7.0-xml liblocale-po-perl gettext;
+sudo apt --assume-yes purge 'php*';
+sudo apt --assume-yes install php7.1 php7.1-xml php7.1-soap php7.1-curl php7.1-zip php7.1-mbstring php7.1-intl php7.1-gd  php7.1-mysql php7.1-sqlite;
 
 cd /var/www/;
 wget https://download.owncloud.org/community/owncloud-10.0.4.zip;

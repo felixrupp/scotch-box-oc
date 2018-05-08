bb2eff18ef6854b6bb75cebcfafdf342d3a12ad2
Felix Rupp <kontakt@felixrupp.com>
2018-05-09 00:02:54 +0200

    Remove custom php version and change hostname

--- install.sh
+++ install.sh
@@ -2,10 +2,10 @@
 
 sudo composer self-update
 
-sudo apt update;
-sudo apt --assume-yes purge 'php*';
-sudo apt --assume-yes install php7.1 php7.1-xml php7.1-soap php7.1-curl php7.1-zip php7.1-mbstring php7.1-intl php7.1-gd  php7.1-mysql php7.1-sqlite;
-sudo apt --assume-yes upgrade;
+#sudo apt update;
+#sudo apt --assume-yes purge 'php*';
+#sudo apt --assume-yes install php7.1 php7.1-xml php7.1-soap php7.1-curl php7.1-zip php7.1-mbstring php7.1-intl php7.1-gd  php7.1-mysql php7.1-sqlite;
+#sudo apt --assume-yes upgrade;
 
 cd /var/www/;
 wget -O owncloud.zip https://download.owncloud.org/community/owncloud-10.0.7.zip;
@@ -22,8 +22,8 @@ sudo touch /etc/apache2/sites-available/owncloud.conf;
 
 sudo tee /etc/apache2/sites-available/owncloud.conf > /dev/null <<'TXT'
 <VirtualHost *:80>
-	ServerName      dev.earth.iem.thm.de
-	ServerAlias     www.dev.earth.iem.thm.de
+	ServerName      oc10.earth.iem.thm.de
+	ServerAlias     www.oc10.earth.iem.thm.de
 	DocumentRoot    "/var/www/owncloud"
 	
 	<Directory /var/www/owncloud/>
